7d85c3ac8b240ae07b9054b8db1773ed4d23fc57
Mithun Balasubramanian <sendmithunb@gmail.com>
2022-09-10 18:31:36 -0500

    Update resources.js

--- components/resources.js
+++ components/resources.js
@@ -1,45 +1,12 @@
-// import { useState } from 'react'
-// import {Avatar, Divider, Flex, Heading, IconButton} from '@chakra-ui/react'
-// import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
-// import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-// import '../assets/ViperPfP.jpg'
-// import NavItem from './navItem'
+import { useState } from "react";
+import { Box } from "@chakra-ui/react";
 
-
-// export default function Resources(){
-//     return (
-//         <>
-//         <Flex
-//             pos='sticky'
-//             left='7'
-//             h='70vh'
-//             marginTop='2.5vh'
-//             boxShadow='0 4px 12px 0 rgba(0,0,0, 0.5)'
-//             w='150px'
-//             flexDir='column'
-//             justifyContents='space-between' 
-//             backgroundColor='blue'
-//         >        
-//             <Flex>
-//                 <Heading mt = {4} alignContent="center"> Icons </Heading>
-//             </Flex>
-//             <Flex
-//                 p='5%'
-//                 flexDir = 'column'
-//                 w = '100%'
-//                 alignText = 'flex-start'
-//                 mb={4}
-//             >
- 
-//                 <Divider/>
-//                 <Flex>
-//                     <NavItem/>
-//                 </Flex>
-                    
-//             </Flex>
-//         </Flex>
-
-
-//         </>
-//     )
-// }
+export default function Resources() {
+  return (
+    <>
+      <Box bg="blue" w="100%" p={4} color="white">
+        Box 1
+      </Box>
+    </>
+  );
+}
