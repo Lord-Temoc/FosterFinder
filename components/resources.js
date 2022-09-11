35ddb039d11150f982a10094e632844b2c279b5d
AishwaryaSudarshan <63026188+AishwaryaSudarshan@users.noreply.github.com>
2022-09-10 19:49:22 -0500

    add box to resources

--- components/resources.js
+++ components/resources.js
@@ -1,12 +1,41 @@
 import { useState } from "react";
 import { Box } from "@chakra-ui/react";
 
-export default function Resources() {
-  return (
-    <>
-      <Box bg="blue" w="100%" p={4} color="white">
-        Box 1
-      </Box>
-    </>
-  );
-}
+
+// export default function Resources(){
+//     return (
+//         <>
+//         <Flex
+//             pos='sticky'
+//             left='7'
+//             h='70vh'
+//             marginTop='2.5vh'
+//             boxShadow='0 4px 12px 0 rgba(0,0,0, 0.5)'
+//             w='150px'
+//             flexDir='column'
+//             justifyContents='space-between' 
+//             backgroundColor='blue'
+//         >        
+//             <Flex>
+//                 <Heading mt = {4} alignContent="center"> Icons </Heading>
+//             </Flex>
+//             <Flex
+//                 p='5%'
+//                 flexDir = 'column'
+//                 w = '100%'
+//                 alignText = 'flex-start'
+//                 mb={4}
+//             >
+ 
+//                 <Divider/>
+//                 <Flex>
+//                     <NavItem/>
+//                 </Flex>
+                    
+//             </Flex>
+//         </Flex>
+
+
+//         </>
+//     )
+// }
