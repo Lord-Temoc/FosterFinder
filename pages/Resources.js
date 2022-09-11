b524bcf55225c608a190ca66cc80c8ec48e781d3
AishwaryaSudarshan <63026188+AishwaryaSudarshan@users.noreply.github.com>
2022-09-11 00:32:42 -0500

    add links to resource page

--- pages/Resources.js
+++ pages/Resources.js
@@ -3,37 +3,45 @@ import Navbar from "../components/navbar";
 import EventAccordion from "../components/eventsComponent";
 import { useSession, signIn, signOut } from "next-auth/react";
 import ProfileComponent from "../components/profileComp";
+//import Search from "../components/search";
+import PostInput from "../components/postinput";
 import { useState } from "react";
-import {
-  Box,
-  Badge,
-  Center,
-  Square,
-  Circle,
-  Grid,
-  GridItem,
-  Flex,
-} from "@chakra-ui/react";
-import GridLayout from "../components/resources";
+import { Box } from "@chakra-ui/react";
+import PostView from "../components/postview";
+
 
 
 export default function Resources() {
+  
   const { data: session } = useSession();
+
   if (session) {
     return (
       <>
         <div>
-          <div>
           <ProfileComponent />
           <Navbar />
-          </div>
-          <GridLayout title="Baby School" description="Learn about babies"/>
-
         </div>
+        <div>
+          <PostInput name={session?.user.name} />
+        </div>
+      <div>
 
-          
+            <PostView name={session?.user.name} />
+      </div>
 
-
+      <Box bg="tomato" w="25%" p={10} color="white">
+        Box 1
+      </Box>
+      <Box bg="tomato" w="25%" p={10} color="white">
+        Box 2
+      </Box>
+      <Box bg="tomato" w="25%" p={10} color="white">
+        Box 3
+      </Box>
+      <Box bg="tomato" w="25%" p={10} color="white">
+        Box 4
+      </Box>
       </>
     );
   }
