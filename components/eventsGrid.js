5b0ef49c11b6d0d5c7a120c400c853a334ac72b0
Mithun Balasubramanian <sendmithunb@gmail.com>
2022-09-11 08:23:18 -0500

    update gitignore

---
+++ components/eventsGrid.js
@@ -0,0 +1,142 @@
+import { Flex, Grid, GridItem, Box } from "@chakra-ui/react";
+import {
+  Fade,
+  ScaleFade,
+  Slide,
+  SlideFade,
+  Button,
+  onToggle,
+  isOpen,
+} from "@chakra-ui/react";
+
+export default function EventsGrid() {
+  return (
+    <div>
+      <Box position="fixed" width="60%" top="20%" marginLeft="20%">
+        <Grid templateColumns="repeat(5, 2fr)" gap={6}>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            <Button onClick={onToggle}>(props.text)</Button>
+            <Fade in={isOpen}>
+              <Box
+                p="40px"
+                color="white"
+                mt="4"
+                bg="teal.500"
+                rounded="md"
+                shadow="md"
+              >
+                Fade
+              </Box>
+            </Fade>
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 2
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 3
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 4
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 5
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 6
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 7
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 8
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 9
+          </GridItem>
+          <GridItem
+            borderRadius={"15"}
+            w="100%"
+            h="20"
+            bg="blue.500"
+            textAlign={"center"}
+            textColor="white"
+          >
+            {" "}
+            Event 10
+          </GridItem>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
--- pages/Events.js
+++ pages/Events.js
@@ -12,9 +12,8 @@ export default function Events() {
         <div>
           <ProfileComponent />
           <Navbar />
-            <EventsGrid />
+          <EventsGrid />
         </div>
-        
       </div>
     </>
   );
