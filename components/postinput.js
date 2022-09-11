633add9f7773495aea776dd306074e6af41d1635
Mithun Balasubramanian <sendmithunb@gmail.com>
2022-09-10 21:28:20 -0500

    Update postinput.js

--- components/postinput.js
+++ components/postinput.js
@@ -1,11 +1,20 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {
   FormControl,
   FormLabel,
   Input,
   Button,
   useToast,
+  Modal,
+  ModalFooter,
+  ModalContent,
+  ModalOverlay,
+  ModalBody,
+  ModalHeader,
+  ModalCloseButton,
+  useDisclosure,
+  Textarea,
 } from "@chakra-ui/react";
 
 export default function PostInput(props) {
@@ -13,8 +22,13 @@ export default function PostInput(props) {
   const [content, setContent] = useState("");
   const toast = useToast();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = useRef(null);
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    onClose();
 
     if (title === "" || content === "") {
       toast({
@@ -55,8 +69,48 @@ export default function PostInput(props) {
   return (
     <div>
       <h1>Post View</h1>
+      <Button onClick={onOpen}>Open Modal</Button>
 
-      <FormControl>
+      <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Title</FormLabel>
+              <Input
+                ref={initialRef}
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleTitleChange}
+                placeholder="Post Title"
+              />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Content</FormLabel>
+              <Textarea
+                type="text"
+                name="content"
+                value={content}
+                onChange={handleContentChange}
+                placeholder="Post content"
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      {/* <FormControl>
         <FormLabel>Title:</FormLabel>
         <Input
           type="text"
@@ -71,8 +125,8 @@ export default function PostInput(props) {
           value={content}
           onChange={handleContentChange}
         />
-        <Input type="submit" value="Submit" onClick={handleSubmit} />
-      </FormControl>
+        <Input type="submit" value="Submit" onClick={handleSubmit} /> */}
+      {/* </FormControl> */}
     </div>
   );
 }
