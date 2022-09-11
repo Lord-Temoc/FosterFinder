d06d77df2eaf23bc0af8a1196cece4ffe87d7c6b
My-Name-Ae-Jeffv1 <51266456+My-Name-Ae-Jeffv1@users.noreply.github.com>
2022-09-11 08:12:01 -0500

    update events page

--- pages/Events.js
+++ pages/Events.js
@@ -2,16 +2,20 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { Flex, Text, Center, Box } from "@chakra-ui/react";
 import ProfileComponent from "../components/profileComp";
 import Navbar from "../components/navbar";
+import EventsGrid from "../components/eventsGrid";
 
 export default function Events() {
-    const {data: session} = useSession();
-       return (
-         <>
-           <div>
-               <ProfileComponent />
-               <Navbar />
-               <EventsGrid />
-           </div>
-         </>
-       ); 
+  const { data: session } = useSession();
+  return (
+    <>
+      <div>
+        <div>
+          <ProfileComponent />
+          <Navbar />
+            <EventsGrid />
+        </div>
+        
+      </div>
+    </>
+  );
 }
