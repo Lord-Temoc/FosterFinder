4c1d6024c07917a94b16501ee81d4331c40d1035
Mithun Balasubramanian <sendmithunb@gmail.com>
2022-09-10 22:34:49 -0500

    update resources

--- components/navItem.js
+++ components/navItem.js
@@ -1,67 +1,108 @@
-import { useState } from 'react'
-import {Avatar, Button, Divider, Flex, Heading, IconButton, ButtonGroup,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-    Link,} from '@chakra-ui/react'
-import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { useState } from "react";
 import {
-
-  } from '@chakra-ui/react'
-import '../assets/ViperPfP.jpg'
-import "../pages/index.js"
-import Router from 'next/router'
+  Avatar,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  IconButton,
+  ButtonGroup,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Link,
+  Icon,
+} from "@chakra-ui/react";
 import {
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverHeader,
-    PopoverBody,
-    PopoverFooter,
-    PopoverArrow,
-    PopoverCloseButton,
-    PopoverAnchor,
-  } from '@chakra-ui/react'
+  PhoneIcon,
+  AddIcon,
+  WarningIcon,
+  CalendarIcon,
+} from "@chakra-ui/icons";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {} from "@chakra-ui/react";
+import "../assets/ViperPfP.jpg";
+import "../pages/index.js";
+import Router from "next/router";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+} from "@chakra-ui/react";
 
+export default function NavItem() {
+  return (
+    <>
+      <Flex mt={30} flexDir="column" w="100%">
+        <Flex>
+          <div class="btn-group">
+            <Popover>
+              <PopoverTrigger PopoverTrigger="hover">
+                <Button
+                  onClick={() => Router.push("/")}
+                  width={125}
+                  colorScheme="blue"
+                  marginTop={5}
+                  marginLeft={0}
+                >
+                  Home
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Home</PopoverHeader>
+              </PopoverContent>
+            </Popover>
 
-export default function NavItem (){
-
-    return(<>
-    <Flex
-        mt={30}
-        flexDir="column"
-        w='100%'
-        >
-              <Flex>
-                    <div class="btn-group">
-                        <Popover>
-                            <PopoverTrigger PopoverTrigger="hover">
-                                <Button onClick={() => Router.push('/')} width={125} colorScheme='blue' marginTop={5} marginLeft={0}>Home</Button>
-                            </PopoverTrigger>
-                            <PopoverContent>
-                                <PopoverArrow />
-                                <PopoverCloseButton />
-                                <PopoverHeader>Home</PopoverHeader>
-                            </PopoverContent>
-                        </Popover>
-                            
-                                
-                                <Button onClick={() => Router.push('/Events')} width={125} colorScheme='blue' marginTop={5} marginLeft={0}>Events</Button>
-                                <Button onClick={() => Router.push('/Resources')} width={125} colorScheme='blue' marginTop={5} marginLeft={0}>Resources</Button>
-                                <Button onClick={() => Router.push('/Contacts')} width={125} colorScheme='blue' marginTop={5} marginLeft={0}>Contacts</Button>
-                    </div>
-                        </Flex>
-
-
+            <div>
+              <Button
+                onClick={() => Router.push("/Events")}
+                width={125}
+                colorScheme="blue"
+                marginTop={5}
+                marginLeft={0}
+              >
+                <CalendarIcon />
+                Events
+              </Button>
+            </div>
+            <div>
+              <Button
+                onClick={() => Router.push("/Resources")}
+                width={125}
+                colorScheme="blue"
+                marginTop={5}
+                marginLeft={0}
+              >
+                Resources
+              </Button>
+            </div>
+            <div>
+              <Button
+                onClick={() => Router.push("/Contacts")}
+                width={125}
+                colorScheme="blue"
+                marginTop={5}
+                marginLeft={0}
+              >
+                Contacts
+              </Button>
+            </div>
+          </div>
         </Flex>
+      </Flex>
     </>
-)
-        
-        
+  );
 }
--- components/resources.js
+++ components/resources.js
@@ -1,7 +1,34 @@
+import Head from "next/head";
+import Navbar from "../components/navbar";
+import EventAccordion from "../components/eventsComponent";
+import { useSession, signIn, signOut } from "next-auth/react";
+import ProfileComponent from "../components/profileComp";
 import { useState } from "react";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Badge,
+  Center,
+  Square,
+  Circle,
+  Grid,
+  GridItem,
+  Flex,
+} from "@chakra-ui/react";
 
-
+export default function Resources() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <div>
+          <ProfileComponent />
+          <Navbar />
+        </div>
+        <div></div>
+      </>
+    );
+  }
+}
 // export default function Resources(){
 //     return (
 //         <>
@@ -13,9 +40,9 @@ import { Box } from "@chakra-ui/react";
 //             boxShadow='0 4px 12px 0 rgba(0,0,0, 0.5)'
 //             w='150px'
 //             flexDir='column'
-//             justifyContents='space-between' 
+//             justifyContents='space-between'
 //             backgroundColor='blue'
-//         >        
+//         >
 //             <Flex>
 //                 <Heading mt = {4} alignContent="center"> Icons </Heading>
 //             </Flex>
@@ -26,15 +53,14 @@ import { Box } from "@chakra-ui/react";
 //                 alignText = 'flex-start'
 //                 mb={4}
 //             >
- 
+
 //                 <Divider/>
 //                 <Flex>
 //                     <NavItem/>
 //                 </Flex>
-                    
+
 //             </Flex>
 //         </Flex>
-
 
 //         </>
 //     )
