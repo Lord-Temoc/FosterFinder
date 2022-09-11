3c5c318d135d6ceec2a51151c3e48e4a7833d9ca
My-Name-Ae-Jeffv1 <51266456+My-Name-Ae-Jeffv1@users.noreply.github.com>
2022-09-11 09:21:57 -0500

    finish events page/ add popups for event

--- components/eventsGrid.js
+++ components/eventsGrid.js
@@ -7,12 +7,19 @@ import {
   Button,
   onToggle,
   isOpen,
+  Popover,
+  PopoverTrigger,
+  PopoverHeader,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverBody,
 } from "@chakra-ui/react";
 
 export default function EventsGrid() {
   return (
     <div>
-      <Box position="fixed" width="60%" top="20%" marginLeft="20%">
+      <Box position="fixed" width="65%" top="20%" marginLeft="17%">
         <Grid templateColumns="repeat(2, 2fr)" gap={6}>
           <GridItem
             borderRadius={"15"}
@@ -21,20 +28,41 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            Event 1
-            <Fade in={isOpen}>
-              <Box
-                p="40px"
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Foster Care and Adoption Meet and Greet (Virtual)
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
                 color="white"
-                mt="4"
-                bg="teal.500"
-                rounded="md"
-                shadow="md"
+                bg="blue.800"
+                borderColor="blue.800"
               >
-                Fade
-              </Box>
-            </Fade>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Sept 20 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  Interested in learning more about foster care and/or adoption?
+                  Unsure of where to start? We can help! Join us for our next
+                  meet and greet.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -43,9 +71,41 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 2
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Trust-Based Relational Intervention
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Sept 26 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  TBRI® is an attachment-based, trauma-informed intervention
+                  that is designed to meet the complex needs of vulnerable
+                  children.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -54,9 +114,41 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 3
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  National Adoption Day 2022
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Nov 22 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  National Adoption Day is a collective effort to raise
+                  awareness of the more than 115,000 children waiting to be
+                  adopted from foster care in the United States.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -65,9 +157,41 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 4
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Trust-Based Relational Intervention
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Nov 26 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  Trust-Based Relational Intervention (TBRI) is a therapeutic
+                  model that trains caregivers to provide effective support and
+                  treatment for at-risk children
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -76,9 +200,40 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 5
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Webinar: Understanding Autism in Girls
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Nov 31 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  This webinar will seek to illuminate information regarding ASD
+                  in girls and why it can often go missed or misunderstood.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -87,9 +242,41 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 6
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Parent Huddle Support Group
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Dec 2 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  Many foster parents are raising very challenging children and
+                  youth — and we need each other’s support as we go through this
+                  journey.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -98,9 +285,40 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 7
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Orphan Initiative Interest Meeting
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Dec 8 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  Join us for coffee, dessert, and fellowship as we discuss our
+                  plans for caring for vulnerable children in our community.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -109,9 +327,40 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 8
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Webinar: Understanding Autism
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Dec 11 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  This webinar will seek to illuminate information regarding ASD
+                  in girls and why it can often go missed or misunderstood.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -120,9 +369,41 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 9
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Webinar: Navigating the Adult Systems
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Dec 16 2022</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  This presentation and conversation will help inform you about
+                  the adult systems and the information needed and questions to
+                  ask
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
           <GridItem
             borderRadius={"15"}
@@ -131,9 +412,42 @@ export default function EventsGrid() {
             bg="blue.500"
             textAlign={"center"}
             textColor="white"
+            fontSize={"25"}
           >
-            {" "}
-            Event 10
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  variant="link "
+                  fontSize={"20"}
+                  bg="blue.500"
+                  textAlign={"center"}
+                  textColor="white"
+                >
+                  Webinar: The Crossroads of ASD and Mental Health
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                color="white"
+                bg="blue.800"
+                borderColor="blue.800"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Dec 19</PopoverHeader>
+                <PopoverBody
+                  border="0"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  pb={4}
+                >
+                  Autism Spectrum Disorder is a neurodevelopmental disorder that
+                  has strong overlap with mental health issues. Those with ASD
+                  are at higher risk for almost every mental health disorder,
+                  except substance abuse.
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </GridItem>
         </Grid>
       </Box>
