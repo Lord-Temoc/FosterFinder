a0528041b15674750989a1382d214a442e3f982b
Aditya Sinha <saditya3d@gmail.com>
2022-09-11 02:17:07 -0500

    eventgrid import error

--- pages/Events.js
+++ pages/Events.js
@@ -2,7 +2,6 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { Flex, Text, Center, Box } from "@chakra-ui/react";
 import ProfileComponent from "../components/profileComp";
 import Navbar from "../components/navbar";
-import EventsGrid from "../components/eventsGrid";
 
 export default function Events() {
     const {data: session} = useSession();
